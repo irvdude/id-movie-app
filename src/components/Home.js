e18636671b9306c84681299c7a21734b1bdebd0e
swrvr <irvdude44@gmail.com>
2023-07-02 22:18:48 -0400

    more fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import '../index.css';
 function Home() {
   return (
     <div>
-        <h1 className='text-green-500'>IMDB TOP 100 MOVIES</h1>
+        <h1>IMDB TOP 100 MOVIES</h1>
         <SearchBar />
     </div>
   )
